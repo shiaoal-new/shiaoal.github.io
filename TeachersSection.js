eb3a91c5b071f6a015fae652497897a1d7f3067a
shiaoal <shiaoal@gmail.com>
2025-08-15 13:02:43 +0800

    add more complete data of teachers

--- TeachersSection.js
+++ TeachersSection.js
@@ -5,77 +5,323 @@ export const TeachersSection = {
         <h2>教師團隊</h2>
         <p>同心華德福的專業教育團隊</p>
       </div>
-      <div class="teachers fade-in">
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-user-tie"></i>
+
+      <!-- 領導團隊 -->
+      <div class="teacher-category fade-in">
+        <h3 class="category-title">
+          <i class="fas fa-crown"></i>
+          領導團隊
+        </h3>
+        <div class="teachers-grid">
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-user-graduate"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>鄭同僚</h4>
+                <p class="position">協會顧問</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>美國威斯康辛迪遜大學哲學博士</li>
+                  <li>美國威斯康辛迪遜大學教育行政學碩士</li>
+                  <li>國立政治大學教育學系專任副教授</li>
+                  <li>臺北市華德福教育推廣協會理事長</li>
+                  <li>臺灣實驗教育推動中心計畫主持人</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div class="teacher-info">
-            <h3>鄭同僚</h3>
-            <p>協會顧問</p>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-user-tie"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>詹雅智</h4>
+                <p class="position">執行長</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>德國斯圖加特自由華德福教育大學優律思美教育碩士</li>
+                  <li>英國Emerson學院人智基礎哲學與英語科系</li>
+                  <li>慈心華德福學校創校教師、班級導師、英語老師</li>
+                  <li>國際人智學與華德福組織臺灣代表</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-user-friends"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>何宜儀</h4>
+                <p class="position">理事長</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>國立臺灣大學心理學系畢</li>
+                  <li>同心華德福實驗教育機構創校家長</li>
+                  <li>明基電通品牌管理部行銷企劃經理</li>
+                  <li>台灣電通廣告公司助理文案指導</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-user-tie"></i>
+      </div>
+
+      <!-- 專任教師群 -->
+      <div class="teacher-category fade-in">
+        <h3 class="category-title">
+          <i class="fas fa-chalkboard-teacher"></i>
+          專任教師群
+        </h3>
+        <div class="teachers-grid">
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-heart"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>黃郁容</h4>
+                <p class="position">教師會主席・G2 琥珀班導師</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="motto">
+                <p>"跨入同心，攜手同心。當親師生都帶著真誠開放的心相遇，願意傾聽對方、向他人學習，便能帶來療癒的力量，支持彼此在實踐華德福教育的路上前行。"</p>
+              </div>
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>臺北市立大學心理與諮商學系學士</li>
+                  <li>國民小學教師資格</li>
+                  <li>曾任同心華德福行政教師、G1～5主帶教師</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div class="teacher-info">
-            <h3>詹雅智</h3>
-            <p>執行長</p>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-cogs"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>吳俊輝</h4>
+                <p class="position">總務兼行政主任</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="motto">
+                <p>"Il est absolument possible qu'au-delà de ce que perçoivent nos sens se cachent des mondes insoupçonnés."</p>
+              </div>
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>法國格勒諾布爾第三大學語言科學系所碩士</li>
+                  <li>法國遠東學院 (EFEO) 台北中心研究助理</li>
+                  <li>國立臺北科技大學建築學系研究助理</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-seedling"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>林品嫻</h4>
+                <p class="position">G1 銀河班導師</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="motto">
+                <p>"期許孩子能隨時懷抱一顆謙卑的心、永遠保持積極上進的態度。面對孩子的困境，我們所能做的是陪伴、關懷並以最大程度的愛與耐心包容。"</p>
+              </div>
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>國立臺灣藝術大學工藝設計學系學士</li>
+                  <li>2018-2022 臺北市同心華德福實驗教育機構 行政</li>
+                  <li>2024-至今 臺北市同心華德福實驗教育機構 主帶老師</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-mountain"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>呂孟庭</h4>
+                <p class="position">G3 山嵐班導師</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="motto">
+                <p>"相信，改變。讓生命彼此引領"</p>
+              </div>
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>國立政治大學教育學系碩士</li>
+                  <li>國民小學教師資格</li>
+                  <li>臺北市同心華德福實驗教育機構1-6年級主帶教師</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-sun"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>周天禧</h4>
+                <p class="position">G4 日初班導師</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="motto">
+                <p>"期許每個孩子在這裡得到所需的力量與光芒，帶著愛與感恩的心，依循自身的渴望，走出屬於自己的道路"</p>
+              </div>
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>華梵大學 美術與文創學系 學士</li>
+                  <li>臺北市同心華德福實驗教育機構1年級主帶教師</li>
+                  <li>國片《樂園》電影劇組 美術助理</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="teacher-card detailed">
+            <div class="teacher-header">
+              <div class="teacher-img">
+                <i class="fas fa-wave-square"></i>
+              </div>
+              <div class="teacher-basic">
+                <h4>游鈞桓</h4>
+                <p class="position">G5 浪花班導師</p>
+              </div>
+            </div>
+            <div class="teacher-details">
+              <div class="motto">
+                <p>"來來來！我的愛～一顆星閃爍亮～一陣海風吹過來～一朵玫瑰開又開～一波浪花捲起來～～"</p>
+              </div>
+              <div class="credentials">
+                <h5>學經歷：</h5>
+                <ul>
+                  <li>國立高雄應用科技大學化學工程與材料系</li>
+                  <li>臺北市同心華德福實驗教育機構1-2年級主帶教師</li>
+                  <li>台北植物園民生作物區園丁</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-user-tie"></i>
+      </div>
+
+      <!-- 兼任教師群 -->
+      <div class="teacher-category fade-in">
+        <h3 class="category-title">
+          <i class="fas fa-users"></i>
+          兼任教師群
+        </h3>
+        <div class="teachers-grid compact">
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-book"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>王振叡</h4>
+              <p>文史</p>
+            </div>
           </div>
-          <div class="teacher-info">
-            <h3>何宜儀</h3>
-            <p>理事長</p>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-leaf"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>陳心恬</h4>
+              <p>自然</p>
+            </div>
           </div>
-        </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-chalkboard-teacher"></i>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-calculator"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>姜怡伶</h4>
+              <p>數學</p>
+            </div>
           </div>
-          <div class="teacher-info">
-            <h3>黃郁容</h3>
-            <p>教師會主席・G2導師</p>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-language"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>班思明 (Mr.BEN)</h4>
+              <p>英文</p>
+            </div>
           </div>
-        </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-chalkboard-teacher"></i>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-palette"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>林祐竹</h4>
+              <p>藝術</p>
+            </div>
           </div>
-          <div class="teacher-info">
-            <h3>吳俊輝</h3>
-            <p>總務兼行政主任</p>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-music"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>陳乃欣</h4>
+              <p>音樂（豎笛）</p>
+            </div>
           </div>
-        </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-chalkboard-teacher"></i>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-running"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>陳瑞宏</h4>
+              <p>體育</p>
+            </div>
           </div>
-          <div class="teacher-info">
-            <h3>廖容</h3>
-            <p>教學</p>
-          </div>
-        </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-chalkboard-teacher"></i>
-          </div>
-          <div class="teacher-info">
-            <h3>董品喬</h3>
-            <p>註冊</p>
-          </div>
-        </div>
-        <div class="teacher-card">
-          <div class="teacher-img">
-            <i class="fas fa-chalkboard-teacher"></i>
-          </div>
-          <div class="teacher-info">
-            <h3>曾本瑜</h3>
-            <p>會計行政</p>
+          <div class="teacher-card compact">
+            <div class="teacher-img">
+              <i class="fas fa-hammer"></i>
+            </div>
+            <div class="teacher-info">
+              <h4>蔡秋樺</h4>
+              <p>木工</p>
+            </div>
           </div>
         </div>
       </div>
